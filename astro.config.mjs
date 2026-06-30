9b2f587a649eea9a5968bcfcaffc2facad43b970
Sam Carrington <84904719+samcarrington@users.noreply.github.com>
2026-06-30 18:07:35 +0100

    feat: external link icon polish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 export default defineConfig({
   srcDir: "./src",
+  integrations: [
+    icon({
+      include: {
+        gridicons: ["external"],
+      },
+    }),
+  ],
 });
