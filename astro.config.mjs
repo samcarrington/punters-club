07d75b4f2dbe7b2668c9d2cd12c1263c596e9ad8
Sam Carrington <84904719+samcarrington@users.noreply.github.com>
2026-07-01 22:16:36 +0100

    fix: implement Astro Image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,14 @@ import icon from "astro-icon";
 
 export default defineConfig({
   srcDir: "./src",
+  image: {
+    domains: [
+      "thumbnailer.mixcloud.com",
+      "pickasso.spotifycdn.com",
+      "mosaic.scdn.co",
+      "resources.tidal.com",
+    ],
+  },
   integrations: [
     icon({
       include: {
