e3a10f1608563a82c12311d49325c2063e81f687
Sam Carrington <84904719+samcarrington@users.noreply.github.com>
2026-07-08 10:06:35 +0100

    feat: add sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,46 @@
+import generatedShowsData from "../data/shows.generated.json";
+import type { Show } from "../lib/mixcloud";
+import { assertValidShowSlugs } from "../lib/shows";
+
+const DEFAULT_SITE_URL = "https://punters.club";
+
+const xmlEscape = (value: string) =>
+  value
+    .replaceAll("&", "&amp;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&apos;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;");
+
+const sitemapEntry = (url: string, changefreq: "weekly" | "monthly") => `  <url>
+    <loc>${xmlEscape(url)}</loc>
+    <changefreq>${changefreq}</changefreq>
+  </url>`;
+
+export function GET({ site }: { site?: URL }) {
+  assertValidShowSlugs(generatedShowsData as Show[]);
+
+  const baseUrl = site ?? new URL(DEFAULT_SITE_URL);
+  const showUrls = (generatedShowsData as Show[]).flatMap((show) =>
+    show.slug ? [new URL(`/shows/${show.slug}/`, baseUrl).toString()] : [],
+  );
+
+  const entries = [
+    sitemapEntry(new URL("/", baseUrl).toString(), "weekly"),
+    sitemapEntry(new URL("/shows/", baseUrl).toString(), "monthly"),
+    ...showUrls.map((url) => sitemapEntry(url, "monthly")),
+  ];
+
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${entries.join("\n")}
+</urlset>
+`,
+    {
+      headers: {
+        "Content-Type": "application/xml; charset=utf-8",
+      },
+    },
+  );
+}
